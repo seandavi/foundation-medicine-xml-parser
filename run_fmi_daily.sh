707df5c950928e5f4e52d8af4e7d390fb53c3abb
Sean Davis <seandavi@gmail.com>
2022-03-04 06:40:07 -0700

    fix copy/move bash stuff

--- run_fmi_daily.sh
+++ run_fmi_daily.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-/home/davsean/.cache/pypoetry/virtualenvs/uccc-foundation-app-qFY2nxrc-py3.9/bin/python /home/davsean/foundation-app/scripts/fmi_report_data.py
-cp -r 20* /mnt/00_results/00_latest
-mv 20* /mnt/00_results/
+#/home/davsean/.cache/pypoetry/virtualenvs/uccc-foundation-app-qFY2nxrc-py3.9/bin/python /home/davsean/foundation-app/scripts/fmi_report_data.py
+cp -r 20* /mnt/00_results/
+latest=`ls -d 20* | head -1`
+echo $latest
+cp -r 20* /mnt/00_results/
+rm -rf /mnt/00_results/00_latest
+mv $latest /mnt/00_results/00_latest
 
